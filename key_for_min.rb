32e58469b3ca966982150c225866fc76174e4094
Michael Jester <m.jester.93@gmail.com>
2020-05-23 01:01:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min = 99999999999999
+  min = 100
   min_key = nil
 
   name_hash.each do |key, value|
