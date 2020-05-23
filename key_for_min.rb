111d113ba400a57f09221b86324365ffe856cf13
Michael Jester <m.jester.93@gmail.com>
2020-05-23 01:05:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,6 +3,7 @@
 
 def key_for_min_value(name_hash)
   smallest_hash_key = nil
+
   name_hash.each do |key, val|
     smallest_hash_key ||= key
     smallest_hash_key = key if val < name_hash[smallest_hash_key]
